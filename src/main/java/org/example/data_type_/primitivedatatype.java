05403f1e154ea277bc09b036c654b5ee00da5327
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-11-22 19:57:40 +0530

    first commit

---
+++ src/main/java/org/example/data_type_/primitivedatatype.java
@@ -0,0 +1,8 @@
+package org.example.data_type_;
+
+public class primitivedatatype {
+    public static void main(String[] args) {
+        String name="Sameena";
+        System.out.println(name);
+    }
+}
