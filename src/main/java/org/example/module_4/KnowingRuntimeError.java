e8d5d90c748c329c771a552ccdeb14422d3184cb
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-12-18 19:41:19 +0530

    update

---
+++ src/main/java/org/example/module_4/KnowingRuntimeError.java
@@ -0,0 +1,8 @@
+package org.example.module_4;
+
+public class KnowingRuntimeError {
+    public static void main(String[] args){
+        int a=10/0;
+        System.out.println(a);
+    }
+}
---
+++ src/main/java/org/example/module_4/KnowingSyntaxError.java
@@ -0,0 +1,8 @@
+package org.example.module_4;
+
+public class KnowingSyntaxError {
+    public static void main(String[] args){
+        int a=10/0;
+        System.out.println(a);
+    }
+}
