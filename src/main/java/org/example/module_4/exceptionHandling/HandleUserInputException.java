50cc15eacb28026ce3357fa90d53b8187d0cb06b
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-12-18 21:02:35 +0530

    update

---
+++ src/main/java/org/example/module_4/exceptionHandling/HandleUserInputException.java
@@ -0,0 +1,26 @@
+package org.example.module_4.exceptionHandling;
+
+import java.util.Scanner;
+
+public class HandleUserInputException {
+    public static void main(String[] args) {
+        System.out.println("program started");
+        Scanner sc=new Scanner(System.in);
+        String[] name={"alice","bob","charlie"};
+        boolean run=true;
+        do{
+            System.out.println("Enter an index to acces the name array[0-2]:");
+            int index=sc.nextInt();
+            try{
+                System.out.println("name at index"+index+" is "+name[index]);
+            }
+            catch(Exception e){
+                System.out.println("Invalid index");
+            }
+            System.out.println("Do you want to continue?(true/false):");
+            run=sc.nextBoolean();
+        }while(run);
+        sc.close();
+        System.out.println("Program ended");
+    }
+}
