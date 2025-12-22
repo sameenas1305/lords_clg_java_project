9082c67735c3748296f9f9bad3f36d5d5061f5ab
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-12-22 20:36:57 +0530

    update

---
+++ src/main/java/org/example/module_5/list/WaysToItrateArrayList.java
@@ -0,0 +1,44 @@
+package org.example.module_5.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class WaysToItrateArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> weekends = new ArrayList<>();
+
+        weekends.add("Saturday");
+        weekends.add("Sunday");
+        weekends.add("Monday");
+        weekends.add("Tuesday");
+        weekends.add("Wednesday");
+        weekends.add("Thursday");
+        weekends.add("Friday");
+
+        System.out.println("Using for loop:");
+        for(int i = 0; i<weekends.size(); i++) {
+            System.out.println("Index " + i + ": " + weekends.get(i));
+        }
+
+        System.out.println("\nUsing enhanced for loop (for-each loop):");
+        for(String day: weekends) {
+            System.out.println(day);
+        }
+
+        System.out.println("\nUsing while loop:");
+        int i = 0;
+        while(i < weekends.size()) {
+            System.out.println("Index " + i + ": " + weekends.get(i));
+            i++;
+        }
+
+        System.out.println("\nUsing iterator:");
+        Iterator<String> iterator = weekends.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        System.out.println("\nUsing forEach method:");
+        weekends.forEach(element -> System.out.println(element));
+    }
+}
