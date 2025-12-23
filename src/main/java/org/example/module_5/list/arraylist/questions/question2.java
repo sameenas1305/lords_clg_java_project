f6d6095daf78076391f9d544cabf506f157cce96
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-12-23 20:12:36 +0530

    complete

--- src/main/java/org/example/module_5/list/arraylist/questions/question2.java
+++ src/main/java/org/example/module_5/list/arraylist/questions/question2.java
@@ -1,4 +1,26 @@
 package org.example.module_5.list.arraylist.questions;
 
+import java.util.ArrayList;
+
 public class question2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> input = new ArrayList<>();
+        input.add(4);
+        input.add(9);
+        input.add(1);
+        input.add(7);
+        input.add(9);
+
+        Integer l=Integer.MIN_VALUE;
+        Integer sl=Integer.MIN_VALUE;
+        for (Integer ele : input) {
+            if(ele >l){
+                sl=l;
+                l= ele;
+            } else if (ele >sl && ele != l){
+                sl= ele;
+            }
+        }
+        System.out.println(sl);
+    }
 }
