42418588517eb25e977742f1567a2547c0d732db
SAMEENA SULTANA <sameenas1305@gmail.com>
2025-12-26 19:22:05 +0530

    questions

---
+++ src/main/java/org/example/module_5/list/linkedlist/questions/question1.java
@@ -0,0 +1,18 @@
+package org.example.module_5.list.linkedlist.questions;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class question1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> input = new LinkedList<>(Arrays.asList(1,2,1,3,2));
+        System.out.println(input);
+        LinkedList<Integer> output = new LinkedList<>();
+        for(Integer i : input){
+            if(!output.contains(i)){
+                output.add(i);
+            }
+        }
+        System.out.println(output);
+    }
+}
---
+++ src/main/java/org/example/module_5/list/linkedlist/questions/question4.java
@@ -0,0 +1,4 @@
+package org.example.module_5.list.linkedlist.questions;
+
+public class question4 {
+}
